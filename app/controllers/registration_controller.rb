17a0550d4cd2bcbf9802c2120180bfe2fa99fa34
Lxvia <oliviarudolf67@gmail.com>
2024-05-01 20:29:06 +0200

    correct

---
+++ app/controllers/registration_controller.rb
@@ -0,0 +1,22 @@
+class RegistrationController < ApplicationController
+    def new
+        @user = User.new
+    end
+
+    def create
+        @city = City.find_or_create_by(city_name: params[:city])
+        @user_new = User.new(user_params)
+        @user_new.city = @city
+
+        if @user_new.save!
+            redirect_to root_path
+        else
+            puts "erreur"
+        end
+    end
+
+    private
+    def user_params
+        params.require(:user).permit(:first_name,:last_name,:email, :description, :age, :password, :password_confirmation)
+    end
+end
