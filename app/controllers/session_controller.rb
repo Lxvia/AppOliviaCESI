a19c09642ee3731080c6c53268711500eb398279
Lxvia <oliviarudolf67@gmail.com>
2024-05-01 18:11:17 +0200

    add controller

---
+++ app/controllers/session_controller.rb
@@ -0,0 +1,17 @@
+class SessionController < ApplicationController
+    def new
+       
+    end
+
+    def create
+        user = User.find_by(email: params[:session][:email].downcase)
+        
+        if user && user.authenticate(params[:session][:password])
+            session[:user_id] = user.id
+            redirect_to root_path
+        else
+            flash.now[:danger] = 'Invalid email/password combination'
+            render 'new'
+        end
+    end
+end
