fff31d36b9b8ffc4d8791d69d539b42d22fba1a1
Lxvia <oliviarudolf67@gmail.com>
2024-05-01 19:38:50 +0200

    correct session controller

--- app/controllers/session_controller.rb
+++ app/controllers/session_controller.rb
@@ -1,17 +1,25 @@
 class SessionController < ApplicationController
-    def new
-       
-    end
 
     def create
-        user = User.find_by(email: params[:session][:email].downcase)
+        user = User.find_by(email: params[:email])
         
-        if user && user.authenticate(params[:session][:password])
+        if user && user.authenticate(params[:password])
             session[:user_id] = user.id
             redirect_to root_path
-        else
-            flash.now[:danger] = 'Invalid email/password combination'
+            
+        else 
+            flash[:danger] = 'Invalid email/password combination'
             render 'new'
         end
+
+    end
+
+    def destroy
+        session.delete(:user_id)
+        redirect_to root_path
+    end
+
+    def new
+       
     end
 end
