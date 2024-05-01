8ffddf64ed4033e81a662bd9a9e4f6a68f5a8a18
Lxvia <oliviarudolf67@gmail.com>
2024-05-01 14:58:41 +0200

    add author

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -2,4 +2,8 @@ class StaticPagesController < ApplicationController
   def home
     @gossips = Gossip.all
   end
+
+  def author
+    @author = User.find_by(first_name:params[:first_name])
+  end
 end
