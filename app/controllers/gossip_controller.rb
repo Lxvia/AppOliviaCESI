6e33035482b7da1fa992debe714bb5db4ae39215
Lxvia <oliviarudolf67@gmail.com>
2024-05-02 17:10:10 +0200

    add edit and update gossip

--- app/controllers/gossip_controller.rb
+++ app/controllers/gossip_controller.rb
@@ -8,6 +8,36 @@ class GossipController < ApplicationController
     end
 
     def create
+        @gossip = Gossip.new(user: current_user, title: params[:title], content: params[:content])
+
+        if @gossip.save
+            redirect_to root_path
+        else
+            render :new
+        end
+    end
+
+    def edit
         
+        @gossip = Gossip.find(params[:id])
+
+        if current_user != @gossip.user
+            redirect_to :root
+        end
+    end
+
+    def update
+
+        @gossip = Gossip.find(params[:id])
+
+        post_params = params.require(:gossip).permit(:title, :content)
+
+        if @gossip.update(post_params)
+            redirect_to gossip_path(@gossip)
+            flash[:succes] = "Gossip updated !"
+        else 
+            render :edit
+            flash[:danger] = "Gossip not updated" 
+        end
     end
 end
